ca047f0c4a39495cd0b113c0a603c19538e15aad
Hashir Shazad <hashir447079@gmail.com>
2024-01-02 10:52:53 +0500

    Made game 2d
    Made it 2d

--- CharacterBody3D_Script.gd
+++ CharacterBody3D_Script.gd
@@ -1,44 +1,38 @@
-extends CharacterBody3D
+extends CharacterBody2D
 
 
-var speed = 5
+var speed = 1000
 var jump_velocity = 4
 var friction = .1
 var g_multiplier = 1.2
 
-@onready var sprite3d = $AnimatedSprite3D
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var sprite = $AnimatedSprite2D
 
-
-
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	# sprite flip
 	if velocity.x > 1:
-		sprite3d.flip_h = 0
+		sprite.flip_h = 0
 	elif velocity.x < -1:
-		sprite3d.flip_h = 1
+		sprite.flip_h = 1
 	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta * g_multiplier
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction)
-		velocity.z = move_toward(velocity.z, 0, friction)
-
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	else:
+		velocity.x = move_toward(velocity.x, 0, friction)
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	else:
+		velocity.y = move_toward(velocity.y, 0, friction)
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	else:
+		velocity.y = move_toward(velocity.y, 0, friction)
+	
+	velocity = velocity.normalized()
 	move_and_slide()
 
 
