168c1dec1720e2754922678d9d3bc3fb5474a921
Hashir Shazad <hashir447079@gmail.com>
2024-01-02 10:57:23 +0500

    Made movement speed faster
    Made movement speed faster

--- CharacterBody3D_Script.gd
+++ CharacterBody3D_Script.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-var speed = 1000
+var speed = 10
 var jump_velocity = 4
 var friction = .1
 var g_multiplier = 1.2
@@ -16,15 +16,13 @@ func _physics_process(_delta):
 		sprite.flip_h = 1
 	
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
+		velocity.x -= speed
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+		velocity.x += speed
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+		velocity.y -= speed
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	
-	velocity = velocity.normalized()
+		velocity.y += speed
 	move_and_slide()
 
 
