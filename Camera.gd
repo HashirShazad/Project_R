d6344d2a2616915a9f9a4a00dad6a3cef79c42c2
Hashir Shazad <hashir447079@gmail.com>
2024-01-02 16:09:49 +0500

    2p

---
+++ Camera.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+
+@onready var P1 = %Character
+@onready var P2 = %Character2
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.position = (P1.position + P2.position)/2
--- CharacterBody3D_Script.gd
+++ CharacterBody3D_Script.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
+@export var btn_up : StringName = "P1_Up"
+@export var btn_down : StringName  = "P1_Down"
+@export var btn_left : StringName = "P1_Left"
+@export var btn_right : StringName = "P1_Right"
 
-var speed = 200
+var walk_speed = 200
+var speed = 150
 var jump_velocity = 4
 var friction = 10
 var g_multiplier = 1.2
@@ -10,13 +15,13 @@ var player_direction :Vector2 = Vector2(0, 0)
 
 func _physics_process(_delta):
 	
-	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_x = Input.get_axis(btn_left, btn_right)
 	if direction_x:
 		velocity.x = direction_x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction)
 		
-	var direction_y = Input.get_axis("ui_up", "ui_down")
+	var direction_y = Input.get_axis(btn_up, btn_down)
 	if direction_y:
 		velocity.y = direction_y * speed
 	else:
