810b3116db226e18b094832b37f1c78028b81b94
Hashir Shazad <hashir447079@gmail.com>
2024-01-02 10:55:04 +0500

    12
    123

--- CharacterBody3D_Script.gd
+++ CharacterBody3D_Script.gd
@@ -17,20 +17,12 @@ func _physics_process(_delta):
 	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	else:
-		velocity.x = move_toward(velocity.x, 0, friction)
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	else:
-		velocity.x = move_toward(velocity.x, 0, friction)
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-	else:
-		velocity.y = move_toward(velocity.y, 0, friction)
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-	else:
-		velocity.y = move_toward(velocity.y, 0, friction)
 	
 	velocity = velocity.normalized()
 	move_and_slide()
