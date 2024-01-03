c6b731378765c9b6bfb69c46a59c5857149795ca
Hashir Shazad <hashir447079@gmail.com>
2024-01-03 07:11:49 +0500

    Arranged

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -7,4 +7,6 @@ extends Camera2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.position = (P1.position + P2.position)/2
+	self.zoom.x =  (abs(P1.position.x - P2.position.x))
+	self.zoom.y =  (abs(P1.position.y - P2.position.y))
 
