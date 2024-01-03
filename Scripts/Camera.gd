7fd55a4ba840aea1c56e28e9f5465010a427c067
Hashir Shazad <hashir447079@gmail.com>
2024-01-03 07:55:33 +0500

    Added_DS

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -4,9 +4,10 @@ extends Camera2D
 @onready var P1 = %Character
 @onready var P2 = %Character2
 
+var const_factor : float = 100.0
+var damp_factor : float = 2.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position = (P1.position + P2.position)/2
-	self.zoom.x =  (abs(P1.position.x - P2.position.x))
-	self.zoom.y =  (abs(P1.position.y - P2.position.y))
+	self.position = ( P1.position + P2.position ) / 2
 
--- Scripts/CharacterBody3D_Script.gd
+++ Scripts/CharacterBody3D_Script.gd
@@ -10,38 +10,58 @@ var speed = 150
 var jump_velocity = 4
 var friction = 10
 var g_multiplier = 1.2
-var player_direction :Vector2 = Vector2(0, 0)
+var direction :Vector2 = Vector2(0, 0)
+var mana : int = 30
+var health : int = 30
+var stamina : int = 30
+
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(_delta):
 	
 	var direction_x = Input.get_axis(btn_left, btn_right)
+	var direction_y = Input.get_axis(btn_up, btn_down)
+	
 	if direction_x:
+		direction.x = direction_x
+		direction.y = direction_y
 		velocity.x = direction_x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction)
-		
-	var direction_y = Input.get_axis(btn_up, btn_down)
 	if direction_y:
+		direction.y = direction_y
+		direction.x = direction_x
 		velocity.y = direction_y * speed
 	else:
 		velocity.y = move_toward(velocity.y, 0, friction)
 	
-	player_direction = Vector2(direction_x,direction_y)
 	
 	move_and_slide()
 	ani_player()
 
 func ani_player():
-	if player_direction.y == 1:
-		sprite.play("Down")
-	elif player_direction.y == -1:
-		sprite.play("Up")
-	elif player_direction.x == 1:
-		sprite.play("Right")
-	elif player_direction.x == -1:
-		sprite.play("Left")
-	
+	if direction.y == 1:
+		sprite.play("Idle_Down")
+		
+	elif direction.y == -1:
+		sprite.play("Idle_Up")
+		
+	elif direction.x == 1:
+		sprite.flip_h = 0
+		
+		if velocity.x > 20:
+			sprite.play("Walk_Right")
+		else:
+			sprite.play("Idle_Right")
+		
+	elif direction.x == -1:
+		sprite.flip_h = 1
+		
+		if velocity.x < -1:
+			sprite.play("Walk_Right")
+		else:
+			sprite.play("Idle_Right")
+		
 	
 
 
