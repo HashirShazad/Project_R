184c924114c43dca662dddd3a6555af457975530
Hashir Shazad <hashir447079@gmail.com>
2024-01-08 12:46:36 +0500

    popo

--- Scenes/Inventory_Interface.gd
+++ Scenes/Inventory_Interface.gd
@@ -1,9 +1,6 @@
 extends Control
-@onready var p1_inv = $P1_Inv
-@onready var p2_inv = $P2_Inv
+@onready var p_inv = $P_Inv
 
-func set_p1_inventory_data(inventory_data: Inventory_Data) -> void:
-	p1_inv.set_inventory_data(inventory_data)
+func set_player_inventory_data(inventory_data: Inventory_Data) -> void:
+	p_inv.set_inventory_data(inventory_data)
 
-func set_p2_inventory_data(inventory_data: Inventory_Data) -> void:
-	p2_inv.set_inventory_data(inventory_data)
--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -2,11 +2,12 @@ extends Node
 
 @onready var p1 = %Character
 @onready var p2 = %Character2
-@onready var inventory_interface = $UI/Inventory_Interface
+@onready var p1_inventory_interface = $Node2D/Character/Inventory_Interface
+@onready var p2_inventory_interface = $Node2D/Character2/Inventory_Interface
 @onready var p1_inv = $Node2D/Character/Inventory_Interface/P_Inv
 @onready var p2_inv = $Node2D/Character2/Inventory_Interface/P_Inv
 
 
 func _ready() -> void:
-	inventory_interface.set_p1_inventory_data(p1.inventory_data)
-	inventory_interface.set_p2_inventory_data(p2.inventory_data)
+	p1_inventory_interface.set_player_inventory_data(p1.inventory_data)
+	p2_inventory_interface.set_player_inventory_data(p2.inventory_data)
